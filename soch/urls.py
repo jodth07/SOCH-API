cb2c0118a2850c2448ffbddd79a08a1b851fa97d
O. Josué <yashiro@MacBook-Pro.local>
2018-11-05 20:46:24 -0500

    added some swagga

--- soch/urls.py
+++ soch/urls.py
@@ -14,10 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path, include
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="dimanche.nav@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+#    validators=['flex', 'ssv'],
+   public=True,
+#    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    # path('api-auth'/, include('rest_framework.urls')),
+    # path('', )
+#     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+#    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+
 ]
