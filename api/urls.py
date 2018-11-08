9b67e22b811f10d729be6f3fde79bff2476e6824
O. Josué <yashiro@MacBook-Pro.local>
2018-11-08 07:52:29 -0500

    updated views, urls, and migrations for style, products, and category

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from api import views
 urlpatterns = [
     path('products/<int:product_id>', views.ProductsView.as_view(), name='id-products'),
     path('products/', views.ProductsView.as_view(), name='all-products'),
+    path('styles/<int:product_id>', views.StylesView.as_view(), name='id-styles'),
+    path('styles/', views.StylesView.as_view(), name='all-styles'),
     path('contacts/<int:contact_id>', views.ContactsView.as_view(), name='id-contacts'),
     path('contacts/', views.ContactsView.as_view(), name='all-contacts'),
     path('group/', views.GroupView.as_view(), name="all-group")
--- api/views.py
+++ api/views.py
@@ -85,6 +85,7 @@ class ContactsView(APIView):
          else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
             
+
 class GroupView (generics.ListCreateAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
@@ -178,3 +179,80 @@ class ProductsView(APIView):
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
         
     
+class StylesView(APIView):
+    """
+    get:
+    Return a list of all existing styles 
+    
+    post:
+    Create a new style 
+    
+    put:
+    Update a style
+    
+    delete:
+    Delete a style
+    """
+    @swagger_auto_schema(
+        responses = {status.HTTP_200_OK : StyleSerializer(many=True)}
+    )
+    def get(self, request, style_id):
+        if style_id is not None:
+            style = Style.objects.get(id=style_id)
+            serializer = StyleSerializer(style, many=True)
+            return Response(serializer.data)
+        else:
+            styles = Style.objects.getAll()
+            serializer = StyleSerializer(styles, many=True)
+            return Response(serializer.data)
+
+    @swagger_auto_schema(
+        request_body = StyleSerializer,
+        responses = {
+            status.HTTP_200_OK : StyleSerializer,
+            status.HTTP_400_BAD_REQUEST: openapi.Response(description="Missing information")
+        }
+    )
+    def post(self, request):
+        serializer = StyleSerializer(data=request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        response = {status.HTTP_204_NO_CONTENT}
+    )
+    def delete(self, request, style_id):
+        style = Style.objects.get(id=style_id)
+        style.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
+
+
+    @swagger_auto_schema(
+        responses = {
+            status.HTTP_200_OK : StyleSerializer,
+            status.HTTP_400_BAD_REQUEST : openapi.Response(description="Missing information")
+        }
+    )
+    def put(self, request, style_id):
+        style = Style.objects.get(id=style_id)
+        style.name = request.data.get("name")
+        style.price = request.data.get("price")
+        style.description = request.data.get("description")
+        style.num_requested = request.data.get("num_requested")
+        style.image = request.data.get("image")
+        style.duration = request.data.get("duration")
+        style.created_date = request.data.get("created_date")
+        style.purchased_date = request.data.get("purchased_date")
+        style.categories = request.data.get("categories")
+        style.save()
+
+        serializer = StyleSerializer(data=request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+      
