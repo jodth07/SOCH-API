59dd54364812b7f45d36b4c4705401baba9c6a9b
O. Josué <yashiro@MacBook-Pro.local>
2018-11-28 19:12:58 -0500

    pulling

--- products/models.py
+++ products/models.py
@@ -1,22 +1,17 @@
-from django.db import models
-from datetime import datetime  
+# products.models
 from decimal import Decimal
-
-# Create your models here.
 from django.db import models
 from django.db.models.signals import post_save
 from rest_framework import serializers
 
+# Local imports
 from images.models import Image, ImageSerializer
 from users.models import User
-
-TAX_PERCENTAGE = 0.07
 
 CATEGORY_CHOICES = (
     ("Style", "Style"),
     ("Product", "Product")
 )
-
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -37,7 +32,6 @@ class Product(models.Model):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # image = ImageSerializer()
     
     class Meta:
         model = Product
@@ -47,21 +41,19 @@ class ProductSerializer(serializers.ModelSerializer):
 class Variation(models.Model):
     title = models.CharField(max_length=120, blank=True)
     category = models.CharField(max_length=7, choices=CATEGORY_CHOICES, default="Product")
-    
     price = models.DecimalField(decimal_places=2, max_digits=20)
     description = models.CharField(max_length=200, default="")
     
     duration = models.IntegerField(default=0, blank=True)
-    
     company = models.CharField(max_length=100, blank=True, default="")
     
     added = models.DateField(auto_now_add=True, blank=True, null=True)
-    purchased_date = models.DateField(blank=True, auto_now=True, null=True) # Added date
+    purchased_date = models.DateField(blank=True, auto_now=True, null=True)
 
     # Relationationals 
     image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True, blank=True)
-   
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
     sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
     inventory = models.IntegerField(null=True, blank=True)
     discount = models.DecimalField(decimal_places=2, max_digits=2, null=True, blank=True) 
